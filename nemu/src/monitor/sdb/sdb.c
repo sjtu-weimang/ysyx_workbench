ae6354b71b3c11fce6e7d43c2de04f48cbbc0a77
tracer-ysyx <tracer@ysyx.org>
2025-01-01 11:47:04 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 5.15.0-127-generic #137~20.04.1-Ubuntu SMP Fri Nov 15 14:46:54 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:47:04 up 15:16,  1 user,  load average: 1.09, 1.04, 1.02

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -58,6 +58,7 @@ static int cmd_siN(char *args){
 static int cmd_info(char *args) {
   char *arg = strtok(NULL, " ");
   if (strcmp(arg, "r") == 0) {
+    printf("info r to be implement\n");
     isa_reg_display();
   }else if (strcmp(arg, "w") == 0) {
     //wp_display();
