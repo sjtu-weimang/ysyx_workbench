38eec721232e8b73127190fe5e58759e7d223d66
tracer-ysyx <tracer@ysyx.org>
2025-01-01 23:20:08 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 5.15.0-127-generic #137~20.04.1-Ubuntu SMP Fri Nov 15 14:46:54 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:20:08 up 1 day,  2:49,  1 user,  load average: 0.86, 0.95, 1.19

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -42,7 +42,7 @@ static struct rule {
   /* TODO: Add more rules.
    * Pay attention to the precedence level of different rules.
    */
-
+  //用正则表达式匹配token
   {" +", TK_NOTYPE},    // 空格
   {"\\+", TK_PLUS},     // 加号
   {"-", TK_MINUS},      // 减号
@@ -143,100 +143,206 @@ int get_precedence(int type){
   }
 }
 
-word_t expr(char *e, bool *success) {
-  if (!make_token(e)) {
-    *success = false;
-    return 0;
+// //使用将中缀表达式转换为后缀表达式的方法计算表达式的值
+// word_t expr(char *e, bool *success) {
+//   if (!make_token(e)) {
+//     *success = false;
+//     return 0;
+//   }
+
+//   Token postfix[32];
+//   int postfix_len = 0;
+//   int op_stack[32];
+//   int op_top = 0;
+
+//   //将中缀表达式转换为后缀表达式
+//   for (int i = 0; i < nr_token; i++) {
+//     Token token = tokens[i];
+
+//     if (token.type == TK_NUM) {
+//       postfix[postfix_len++] = token;
+//     } else if (token.type == TK_LPAR) {
+//       op_stack[op_top++] = token.type;
+//     } else if (token.type == TK_RPAR) {
+//       while (op_top > 0 && op_stack[op_top - 1] != TK_LPAR) {
+//         postfix[postfix_len++] = (Token){op_stack[op_top - 1], ""};
+//         op_top--;
+//       }
+//       if (op_top > 0 && op_stack[op_top - 1] == TK_LPAR)
+//         op_top--;
+//       else {
+//         *success = false;
+//         return 0;
+//       }
+//     } else if (is_operator(token.type)) {
+//       while (op_top > 0 && op_stack[op_top - 1] != TK_LPAR && get_precedence(op_stack[op_top - 1]) >= get_precedence(token.type)) {
+//         postfix[postfix_len++] = (Token){op_stack[op_top - 1], ""};
+//         op_top--;
+//       }
+//       op_stack[op_top++] = token.type;
+//     }
+//   }
+
+//   while (op_top > 0) {
+//     if (op_stack[op_top - 1] == TK_LPAR || op_stack[op_top - 1] == TK_RPAR) {
+//       *success = false;
+//       return 0;
+//     }
+//     postfix[postfix_len++] = (Token){op_stack[op_top - 1], ""};
+//     op_top--;
+//   }
+
+//   int value_stack[32];
+//   int value_top = 0;
+
+//   //计算后缀表达式的值
+//   for (int i = 0; i < postfix_len; i++) {
+//     Token token = postfix[i];
+
+//     if (token.type == TK_NUM) {
+//       int num = atoi(token.str);
+//       value_stack[value_top++] = num;
+//     } else if (is_operator(token.type)) {
+//       if (value_top < 2) {
+//         *success = false;
+//         return 0;
+//       }
+//       int b = value_stack[--value_top];
+//       int a = value_stack[--value_top];
+//       int result;
+//       switch (token.type) {
+//         case TK_PLUS:
+//           result = a + b;
+//           break;
+//         case TK_MINUS:
+//           result = a - b;
+//           break;
+//         case TK_MUL:
+//           result = a * b;
+//           break;
+//         case TK_DIV:
+//           if (b == 0) {
+//             *success = false;
+//             return 0;
+//           }
+//           result = a / b;
+//           break;
+//         default:
+//           *success = false;
+//           return 0;
+//       }
+//       value_stack[value_top++] = result;
+//     }
+//   }
+
+//   if (value_top != 1) {
+//     *success = false;
+//     return 0;
+//   }
+
+//   *success = true;
+//   return value_stack[0];
+// }
+
+
+
+//检查表达式是否被一对匹配的括号包围
+bool check_parenteses(int p,int q){
+  if(tokens[p].type!='('||tokens[q].type!=')'){
+    return false;
   }
+  int balance=0;
+  for(int i=p;i<=q;i++){
+    if(tokens[i].type=='(')balance++;
+    if(tokens[i].type==')')balance--;
+    if(balance==0 && i!=q) return false;
+  }
+  return true;
+}
 
-  Token postfix[32];
-  int postfix_len = 0;
-  int op_stack[32];
-  int op_top = 0;
+//查找主运算符的位置
+int find_main_op(int p,int q){
+  int balance=0;
+  int main_op_pos=-1;
+  int min_prority=9999;//优先级最低的运算符
 
-  for (int i = 0; i < nr_token; i++) {
-    Token token = tokens[i];
+  for(int i=p;i<=q;i++){
+    if(tokens[i].type=='(')balance++;
+    if(tokens[i].type==')')balance--;
+    if(balance==0)continue;  //忽略括号内的运算符
 
-    if (token.type == TK_NUM) {
-      postfix[postfix_len++] = token;
-    } else if (token.type == TK_LPAR) {
-      op_stack[op_top++] = token.type;
-    } else if (token.type == TK_RPAR) {
-      while (op_top > 0 && op_stack[op_top - 1] != TK_LPAR) {
-        postfix[postfix_len++] = (Token){op_stack[op_top - 1], ""};
-        op_top--;
-      }
-      if (op_top > 0 && op_stack[op_top - 1] == TK_LPAR)
-        op_top--;
-      else {
-        *success = false;
-        return 0;
-      }
-    } else if (is_operator(token.type)) {
-      while (op_top > 0 && op_stack[op_top - 1] != TK_LPAR && get_precedence(op_stack[op_top - 1]) >= get_precedence(token.type)) {
-        postfix[postfix_len++] = (Token){op_stack[op_top - 1], ""};
-        op_top--;
-      }
-      op_stack[op_top++] = token.type;
+    int priority=9999;
+    switch(tokens[i].type){
+      case '+':
+      case '-':
+      priority=1;
+      break;
+      case '*':
+      case '/':
+      priority=2;
+      break;
+    } 
+    if(priority<=min_prority){
+      min_prority=priority;
+      main_op_pos=i;
     }
   }
+  return main_op_pos;
+}
 
-  while (op_top > 0) {
-    if (op_stack[op_top - 1] == TK_LPAR || op_stack[op_top - 1] == TK_RPAR) {
-      *success = false;
+//递归计算表达式的值
+int eval(int p,int q){
+  if(p>q){
+    //无效表达式
+    printf("Bad expresssion.\n");
+    return 0;
+  }else if(p==q){
+    //单个token，必须是数字
+    if(tokens[p].type==TK_NUM){
+      int val=atoi(tokens[p].str);
+      return val;
+    }else{
+      printf("Expected a number,but got: %s\n",tokens[p].str);
       return 0;
     }
-    postfix[postfix_len++] = (Token){op_stack[op_top - 1], ""};
-    op_top--;
-  }
+  }else if(check_parenteses(p,q)){
+    //递归计算去掉括号后的表达式
+    return eval(p+1,q-1);
+  }else{
+    //查找主运算符
+    int op_pos=find_main_op(p,q);
+    if(op_pos==-1){
+      printf("No main operator found.\n");
+      return 0;
+    }
 
-  int value_stack[32];
-  int value_top = 0;
+    int val1=eval(p,op_pos-1);
+    int val2=eval(op_pos+1,q);
 
-  for (int i = 0; i < postfix_len; i++) {
-    Token token = postfix[i];
-
-    if (token.type == TK_NUM) {
-      int num = atoi(token.str);
-      value_stack[value_top++] = num;
-    } else if (is_operator(token.type)) {
-      if (value_top < 2) {
-        *success = false;
-        return 0;
-      }
-      int b = value_stack[--value_top];
-      int a = value_stack[--value_top];
-      int result;
-      switch (token.type) {
-        case TK_PLUS:
-          result = a + b;
-          break;
-        case TK_MINUS:
-          result = a - b;
-          break;
-        case TK_MUL:
-          result = a * b;
-          break;
-        case TK_DIV:
-          if (b == 0) {
-            *success = false;
-            return 0;
-          }
-          result = a / b;
-          break;
-        default:
-          *success = false;
+    switch(tokens[op_pos].type){
+      case '+':return val1+val2;break;
+      case '-':return val1-val2;break;
+      case '*':return val1*val2;break;
+      case '/':
+        if(val2==0){
+          printf("Division by zero.\n");
           return 0;
-      }
-      value_stack[value_top++] = result;
+        }
+        return val1/val2;
+      default:
+        printf("Unknown operator: %s\n",tokens[op_pos].str);
+        return 0;
     }
   }
 
-  if (value_top != 1) {
+}
+
+//使用递归的方式计算表达式的值
+word_t expr(char *e, bool *success){
+    if (!make_token(e)) {
     *success = false;
     return 0;
   }
-
-  *success = true;
-  return value_stack[0];
+  int p=0,q=nr_token;
+  return eval(p,q);
 }
