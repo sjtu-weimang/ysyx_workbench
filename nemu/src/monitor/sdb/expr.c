6bdb8221e2628b2f05f09db6f006e24b1139a065
tracer-ysyx <tracer@ysyx.org>
2025-01-02 09:35:31 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 5.15.0-127-generic #137~20.04.1-Ubuntu SMP Fri Nov 15 14:46:54 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:35:31 up 1 day, 13:04,  1 user,  load average: 0.89, 0.94, 0.94

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -260,36 +260,37 @@ bool check_parenteses(int p,int q){
   return true;
 }
 
-//查找主运算符的位置
-int find_main_op(int p,int q){
-  int balance=0;
-  int main_op_pos=-1;
-  int min_prority=9999;//优先级最低的运算符
+// 查找主运算符的位置
+int find_main_op(int p, int q) {
+  int balance = 0;
+  int main_op_pos = -1;
+  int min_priority = 9999; // 优先级最低的运算符
 
-  for(int i=p;i<=q;i++){
-    if(tokens[i].type=='(')balance++;
-    if(tokens[i].type==')')balance--;
-    if(balance==0)continue;  //忽略括号内的运算符
+  for (int i = p; i <= q; i++) {
+    if (tokens[i].type == '(') balance++;
+    if (tokens[i].type == ')') balance--;
+    if (balance != 0) continue; // 忽略括号内的运算符
 
-    int priority=9999;
-    switch(tokens[i].type){
+    int priority = 9999;
+    switch (tokens[i].type) {
       case '+':
       case '-':
-      priority=1;
-      break;
+        priority = 1; // 加减法优先级最低
+        break;
       case '*':
       case '/':
-      priority=2;
-      break;
-    } 
-    if(priority<=min_prority){
-      min_prority=priority;
-      main_op_pos=i;
+        priority = 2; // 乘除法优先级较高
+        break;
+    }
+
+    if (priority <= min_priority) {
+      min_priority = priority;
+      main_op_pos = i;
     }
   }
+
   return main_op_pos;
 }
-
 //递归计算表达式的值
 int eval(int p,int q){
   if(p>q){
