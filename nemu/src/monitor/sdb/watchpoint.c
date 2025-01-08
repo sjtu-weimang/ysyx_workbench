5f9ea1819469e03e1cf0022a92a79f9042419ebf
tracer-ysyx <tracer@ysyx.org>
2025-01-08 21:50:47 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 5.15.0-127-generic #137~20.04.1-Ubuntu SMP Fri Nov 15 14:46:54 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:50:47 up 11:30,  1 user,  load average: 1.15, 0.70, 0.62

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -41,7 +41,7 @@ bool check_watchpoint(WP **point){
   //point是指针的指针，用来指示引发中断的变量
   //假设观察点的变量都是无符号整数
     static WP* last=NULL;
-          bool *success=NULL;
+    bool success=true;
     static uint32_t last_value=0x80000000;//代指一个未初始化的数值
    if(last==NULL){
      last=head;
@@ -52,7 +52,7 @@ bool check_watchpoint(WP **point){
 
       uint32_t cur_value;
       
-      cur_value=expr(cur->expression,success);
+      cur_value=expr(cur->expression,&success);
       cur_value=1;
       //printf("cur_expression: %s",cur->expression);
       printf("cur_value:%d\n",cur_value);
