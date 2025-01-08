6efb5ca4e7f3db1996a03aed3e1475fa63f197c9
tracer-ysyx <tracer@ysyx.org>
2025-01-08 21:35:49 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 5.15.0-127-generic #137~20.04.1-Ubuntu SMP Fri Nov 15 14:46:54 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:35:49 up 11:15,  1 user,  load average: 0.44, 0.63, 0.62

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -40,13 +40,12 @@ bool check_watchpoint(WP **point){
   //用函数内部的静态变量保存上一次返回的位置
   //point是指针的指针，用来指示引发中断的变量
   //假设观察点的变量都是无符号整数
-   static WP* last=NULL;
-  static uint32_t last_value=34643;//代指一个未初始化的数值
+    static WP* last=NULL;
+    static uint32_t last_value=34643;//代指一个未初始化的数值
    if(last==NULL){
      last=head;
    }
     WP  *cur=last;
-    int n=0;
     while (cur){
     if (cur->expression){
       //bool *success=NULL;
@@ -54,7 +53,7 @@ bool check_watchpoint(WP **point){
       
       //cur_value=expr(cur->expression,success);
       cur_value=1;
-      printf("cur_expression: %s",cur->expression);
+      //printf("cur_expression: %s",cur->expression);
       printf("cur_value:%d\n",cur_value);
       //如果观察点的变量发生变化，则引发中断
       if(cur_value!=last_value){
@@ -66,8 +65,6 @@ bool check_watchpoint(WP **point){
       return true;}
     }
     cur = cur->next;
-    n++;
-    printf("n:%d\n",n);
   }
   return false;
 }
