7bbd22eeb6ff997bb032e3360335759da6126f48
tracer-ysyx <tracer@ysyx.org>
2025-01-08 20:41:27 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 5.15.0-127-generic #137~20.04.1-Ubuntu SMP Fri Nov 15 14:46:54 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:41:27 up 10:21,  1 user,  load average: 0.45, 0.57, 0.65

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -50,6 +50,7 @@ bool check_watchpoint(WP **point){
     if (cur->expression){
       bool *success=NULL;
       uint32_t cur_value;
+      printf("cur_expression:%s\n",cur->expression);
       cur_value=expr(cur->expression,success);
       if(cur_value!=last_value){
       last_value=cur_value;
