37e6cf553f2c7c59b441df5b65af98f9dad493ef
tracer-ysyx <tracer@ysyx.org>
2025-01-08 21:44:51 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 5.15.0-127-generic #137~20.04.1-Ubuntu SMP Fri Nov 15 14:46:54 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:44:51 up 11:24,  1 user,  load average: 0.54, 0.62, 0.63

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -165,9 +165,10 @@ word_t expr(const char *e, bool *success) {
     *success = false;
     return 0;
   }
-  printf("start parsing .....\n");
+  //printf("start parsing .....\n");
   *success = true;
   int position = 0;
+  printf("%d",nr_token);
   int ans = eval(0, nr_token - 1, success, &position);
   if (!*success){
     printf("some problem happens at position %d\n%s\n%*.s^\n", position, e, position, "");
