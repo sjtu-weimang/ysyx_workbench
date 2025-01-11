b7ebe22f40faa902ddb175e9c9a156777dc6b37f
tracer-ysyx <tracer@ysyx.org>
2025-01-11 09:42:55 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 5.15.0-130-generic #140-Ubuntu SMP Wed Dec 18 17:59:53 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:42:55 up 10:48,  1 user,  load average: 0.58, 0.53, 0.56

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -54,11 +54,10 @@ static int cmd_siN(char *args){
 }
 //表达式求值的命令
 static int cmd_p(char *args) {
-  char *arg = strtok(NULL, " ");
   bool success = true;
-  word_t result = expr(arg, &success);
+  word_t result = expr(args, &success);
   if (success) {
-    printf("%s= %u\e[0m\n",args,result);
+    printf("%s= \e[1;36m%u\e[0m\n",args,result);
   } else {
     printf("Invalid expression\n");
   }
