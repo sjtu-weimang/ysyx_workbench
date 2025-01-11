fa53d245eb9493d47822d92a6c32c4974edd5a56
tracer-ysyx <tracer@ysyx.org>
2025-01-11 16:50:57 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 5.15.0-130-generic #140-Ubuntu SMP Wed Dec 18 17:59:53 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:50:57 up 17:56,  1 user,  load average: 0.45, 0.73, 0.77

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -19,6 +19,8 @@ void init_monitor(int, char *[]);
 void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
+word_t expr(char *e,bool *success);
+void test_cmd_p();
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
@@ -27,9 +29,33 @@ int main(int argc, char *argv[]) {
 #else
   init_monitor(argc, argv);
 #endif
-
+  test_cmd_p();
   /* Start engine. */
   engine_start();
 
   return is_exit_status_bad();
 }
+ void test_cmd_p(){
+  int counter=0;
+  char buffer[65535];
+  char *expression;
+  FILE *fp=fopen("output.txt","r");
+  assert(fp!=NULL);
+
+  char* input=fgets(buffer,ARRLEN(buffer),fp);
+    while(input!=NULL){
+      input[strlen(input)-1]='\0';//将末尾的'\n'变成'\0'
+      uint32_t ans=0;
+      bool success=false;
+      char* ans_text=strtok(input," ");
+      sscanf(ans_text,"%u",&ans);
+      expression=input+strlen(ans_text)+1;
+      IFDEF(CONFIG_DEBUG,LOG("Testing %u %s .....\n",ans,expression));
+      uint32_t result=expr(expression,&success);
+      Assert(result==ans,"the result for expression %s is wrong",expression);
+      input=fgets(buffer,ARRLEN(buffer),fp);
+      ++counter;
+    }
+    Log("通过%d个测试用例",counter);
+  
+ }
