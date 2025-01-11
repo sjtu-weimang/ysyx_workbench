047175ad70058a0f741d63c3f1bf676fd701a903
tracer-ysyx <tracer@ysyx.org>
2025-01-11 17:14:20 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 5.15.0-130-generic #140-Ubuntu SMP Wed Dec 18 17:59:53 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:14:20 up 18:19,  1 user,  load average: 0.77, 0.51, 0.59

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -48,9 +48,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
     printf("stopped at WatchPoint(NO.%d:%s)\n",point->NO,point->expression);
     puts(_this->logbuf);
     nemu_state.state=NEMU_STOP;
-    if(cpu.pc==0x8000000c){
-      nemu_state.state=NEMU_QUIT;
-    }
+    // if(cpu.pc==0x8000000c){
+    //   nemu_state.state=NEMU_QUIT;
+    // }
   }
   #endif
 }
@@ -129,7 +129,9 @@ void cpu_exec(uint64_t n) {
   g_timer += timer_end - timer_start;
 
   switch (nemu_state.state) {
-    case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
+    case NEMU_RUNNING:
+    case NEMU_STOP:
+     nemu_state.state = NEMU_STOP; break;
 
     case NEMU_END: case NEMU_ABORT:
       Log("nemu: %s at pc = " FMT_WORD,
