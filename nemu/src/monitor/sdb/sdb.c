ee62862305ef96269864346462bf6992245ed0a9
tracer-ysyx <tracer@ysyx.org>
2025-01-11 09:45:12 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 5.15.0-130-generic #140-Ubuntu SMP Wed Dec 18 17:59:53 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:45:12 up 10:50,  1 user,  load average: 0.97, 0.70, 0.62

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -57,7 +57,7 @@ static int cmd_p(char *args) {
   bool success = true;
   word_t result = expr(args, &success);
   if (success) {
-    printf("%s=\n",args);
+    //printf("%s=\n",args);
     printf("%u\n",result);
   } else {
     printf("Invalid expression\n");
