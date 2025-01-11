4f793c2e9e49bedaf091285ca2dd7682eadbb87e
tracer-ysyx <tracer@ysyx.org>
2025-01-11 09:18:57 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 5.15.0-130-generic #140-Ubuntu SMP Wed Dec 18 17:59:53 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:18:57 up 10:24,  1 user,  load average: 0.59, 0.66, 0.72

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -53,6 +53,7 @@ count:
 	@echo "Count functions in .c and .h files ...."
 	@find . \( -name "*.c" -o -name "*.h" \) -exec cat {} + | grep -c '.*'
 
-conutNoun:
-	@echo "Count functions in .c and .h files ... No Have space"
-	@find . \( -name "*.c" -o -name "*.h"\) -exec cat {} + | grep -cE '^.+$$'
+countNoun:
+	@echo "Counting functions in .c and .h files...No Have Space"
+	@find . \( -name "*.c" -o -name "*.h" \) -exec cat {} + | grep -cE '^.+$$'
+
