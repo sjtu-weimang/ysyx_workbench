88c809f3a166b4569099a676635d34d174bb1497
tracer-ysyx <tracer@ysyx.org>
2025-01-17 16:11:01 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 5.15.0-130-generic #140-Ubuntu SMP Wed Dec 18 17:59:53 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:11:01 up 19:12,  1 user,  load average: 0.42, 0.43, 0.54

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -83,15 +83,15 @@ static bool make_token(const char *e) {
     for (i = 0; i < NR_REGEX; i ++) {
       //rm_so 匹配串的起始位置，rm_eo 匹配串的结束位置
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
-        //const char *substr_start = e + position;
+        const char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
         
         if (substr_len > 32){
           assert(0);
         }
         //打印匹配信息
-        //Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-        //  i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+         i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
         position += substr_len;
         
