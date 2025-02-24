702a36b37fe23a820cd2bbdc13aa5b87d40e013f
tracer-ysyx <tracer@ysyx.org>
2025-02-24 15:23:04 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:23:04 up 1 day,  3:32,  1 user,  load average: 0.30, 0.52, 0.44

--- abstract-machine/klib/build/riscv32e-nemu/src/stdio.d
+++ abstract-machine/klib/build/riscv32e-nemu/src/stdio.d
@@ -3,5 +3,5 @@
  /home/zhang/ysyx-workbench/abstract-machine/am/include/am.h \
  /home/zhang/ysyx-workbench/abstract-machine/am/include/arch/riscv.h \
  /home/zhang/ysyx-workbench/abstract-machine/am/include/amdev.h \
- /home/zhang/ysyx-workbench/abstract-machine/klib/include/klib.h \
- /home/zhang/ysyx-workbench/abstract-machine/klib/include/klib-macros.h
+ /home/zhang/ysyx-workbench/abstract-machine/klib/include/klib-macros.h \
+ /home/zhang/ysyx-workbench/abstract-machine/klib/include/klib.h
