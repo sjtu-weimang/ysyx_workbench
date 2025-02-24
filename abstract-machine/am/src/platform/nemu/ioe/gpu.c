f3a1812547fce0534c5556d6de0cc15a1f03257d
tracer-ysyx <tracer@ysyx.org>
2025-02-24 17:19:41 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:19:41 up 1 day,  5:29,  1 user,  load average: 0.76, 0.70, 0.61

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,26 +4,16 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-
+  // uint32_t screen_wh = inl(VGACTL_ADDR);
+  // uint32_t h = screen_wh & 0xffff;
+  // uint32_t w = screen_wh >> 16;
   // int i;
-  // int w = io_read(AM_GPU_CONFIG).width;
-  // int h = io_read(AM_GPU_CONFIG).height;
   // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // for (i = 0; i < w * h; i++)
-  //   fb[i] = i;
+  // for (i = 0; i < w * h; i ++) fb[i] = i;
   // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  // uint32_t tmp = inl(VGACTL_ADDR);
-  // uint16_t h = (uint16_t)(tmp & 0x0000ffff);
-  // uint16_t w = (uint16_t)(tmp >> 16);
-  // *cfg = (AM_GPU_CONFIG_T) {
-  //   .present = true, .has_accel = false,
-  //   .width = w, .height = h,
-  //   .vmemsz = 0
-  // };
-
   uint32_t screen_wh = inl(VGACTL_ADDR);
   uint32_t h = screen_wh & 0xffff;
   uint32_t w = screen_wh >> 16;
@@ -49,18 +39,8 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
-  // int w = io_read(AM_GPU_CONFIG).width;
-  // uint32_t *dst = (uint32_t *)(uintptr_t) FB_ADDR;
-  // uint32_t *src = (uint32_t *)(uintptr_t) ctl -> pixels;
-  // // 对应位置像素点的信息
-  // for(int i = 0; i < ctl -> h; i++) {
-  //   for(int j = 0; j < ctl -> w; j++) {
-  //     dst[(ctl -> y) * w + i * w + ctl -> x + j] = src[i * (ctl -> w) + j];
-  //   }
-  // }
-  // if (ctl->sync) {
-  //   outl(SYNC_ADDR, 1);
-  // }
 }
 
-void __am_gpu_status(AM_GPU_STATUS_T *status) { status->ready = true; }
+void __am_gpu_status(AM_GPU_STATUS_T *status) {
+  status->ready = true;
+}
