79f516c35998e71de75ff0f1490214a406c26d9b
tracer-ysyx <tracer@ysyx.org>
2025-02-24 13:38:56 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:38:56 up 1 day,  1:48,  1 user,  load average: 0.99, 0.95, 0.80

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -18,25 +18,41 @@
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
-__EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+#define NGPR 32
+
+struct diff_context_t {
+  word_t gpr[32];
+  word_t pc;
+};
+
+void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
+  if (direction == DIFFTEST_TO_REF) {
+    for (size_t i = 0; i < n; i++) {
+      paddr_write(addr + i, 1, *((uint8_t *)buf + i));
+    }
+  } else {
+    assert(0);
+  }
 }
 
-__EXPORT void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+void difftest_regcpy(void *dut, bool direction) {
+  struct diff_context_t *ctx = (struct diff_context_t *)dut;
+  if (direction == DIFFTEST_TO_REF) {
+    for (int i = 0; i < NGPR; i++) {
+      cpu.gpr[i] = ctx->gpr[i];
+    }
+  } else {
+    for (int i = 0; i < NGPR; i++) {
+      ctx->gpr[i] = cpu.gpr[i];
+    }
+  }
 }
 
-__EXPORT void difftest_exec(uint64_t n) {
-  assert(0);
-}
+void difftest_exec(uint64_t n) { cpu_exec(n); }
 
-__EXPORT void difftest_raise_intr(word_t NO) {
-  assert(0);
-}
+void difftest_raise_intr(word_t NO) { assert(0); }
 
-__EXPORT void difftest_init(int port) {
-  void init_mem();
-  init_mem();
+void difftest_init(int port) {
   /* Perform ISA dependent initialization. */
   init_isa();
 }
