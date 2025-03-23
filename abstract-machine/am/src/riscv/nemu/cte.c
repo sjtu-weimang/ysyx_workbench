1af639115c048f170cc1be190a28d0671c53a84a
tracer-ysyx <tracer@ysyx.org>
2025-03-23 21:36:22 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:36:22 up 2 days,  3:05,  1 user,  load average: 0.35, 0.73, 0.90

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,29 +7,17 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-    if (c->GPR1 == -1) {
+    switch (c->mcause) {
+    case 11:
       ev.event = EVENT_YIELD;
-    } else if (c->GPR1 >= 0 && c->GPR1 <= 19) {
-      ev.event = EVENT_SYSCALL;
-    } else if (c->mcause == 0x80000007) {
-      ev.event = EVENT_IRQ_TIMER;
-    } else {
-      ev.event = EVENT_ERROR;
-    }
-    c = user_handler(ev, c);
-
-    // switch (c->mcause) {
-    //   default: ev.event = EVENT_ERROR; break;
-    // }
-
-    switch (ev.event) {
-    case EVENT_PAGEFAULT:
-    case EVENT_ERROR:
+      c->mepc += 4;
       break;
     default:
-      c->mepc += 4;
+      ev.event = EVENT_ERROR;
+      break;
     }
 
+    c = user_handler(ev, c);
     assert(c != NULL);
   }
 
@@ -49,18 +37,17 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
-  Context *kctx = (Context *)(kstack.end - sizeof(Context));
-
-  memset(kctx, 0, sizeof(kctx));
-  kctx->mepc = (uintptr_t)entry;
-  kctx->mstatus = 0x1800;
-  kctx->GPRx = (uintptr_t)arg;
-  return kctx;
+  Context *ctx = kstack.end - sizeof(Context);
+  ctx->mepc = (uintptr_t)entry;
+  ctx->mstatus = 0x1800;
+  ctx->mcause = 11;
+  ctx->gpr[10 /*a0*/] = (uintptr_t)arg;
+  ctx->gpr[2 /*sp*/] = (uintptr_t)(kstack.end - sizeof(Context));
+  return ctx;
 }
 
 void yield() {
 #ifdef __riscv_e
-
   asm volatile("li a5, -1; ecall");
 #else
   asm volatile("li a7, -1; ecall");
@@ -71,5 +58,4 @@ bool ienabled() {
   return false;
 }
 
-void iset(bool enable) {
-}
+void iset(bool enable) {}
