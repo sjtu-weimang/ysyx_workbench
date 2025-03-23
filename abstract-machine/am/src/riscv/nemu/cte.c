69486da813f14c3435e79691112bac5b49288297
tracer-ysyx <tracer@ysyx.org>
2025-03-23 11:53:53 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:53:53 up 1 day, 17:23,  1 user,  load average: 0.52, 0.73, 0.82

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -37,13 +37,12 @@ Context* __am_irq_handle(Context *c) {
 extern void __am_asm_trap(void);
 
 bool cte_init(Context*(*handler)(Event, Context*)) {
+  // register event handler
+  user_handler = handler;
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
-  // register event handler
-  user_handler = handler;
-
-  return true;
+   return true;
 }
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
