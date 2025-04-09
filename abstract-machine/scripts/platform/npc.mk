e169ee29136d1cb8efc12f36b8408b81010f7204
tracer-ysyx <tracer@ysyx.org>
2025-04-09 22:01:55 +0800

    > sim RTL ysyx_22040000 张三 Linux zhang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:01:55 up 10:53,  1 user,  load average: 0.63, 0.77, 0.75

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -14,6 +14,7 @@ LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
 CFLAGS += -I/usr/include/SDL2
 LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
+LDFLAGS  += -L/usr/lib/libSDL2* -lSDL2
 NPCFLAGS = +trace batch-on
 
 MAINARGS_MAX_LEN = 64
