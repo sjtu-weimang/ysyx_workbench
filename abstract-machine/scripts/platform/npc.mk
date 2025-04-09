108e41bd53bf643b3b44de4d4114499a75d70926
tracer-ysyx <tracer@ysyx.org>
2025-04-09 17:21:32 +0800

    > sim RTL ysyx_22040000 张三 Linux zhang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:21:32 up  6:12,  1 user,  load average: 0.32, 0.47, 0.70

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -13,7 +13,7 @@ CFLAGS    += -fdata-sections -ffunction-sections
 LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
 LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
-NPCFLAGS = +trace
+NPCFLAGS = +trace batch-on
 
 MAINARGS_MAX_LEN = 64
 MAINARGS_PLACEHOLDER = The insert-arg rule in Makefile will insert mainargs here.
