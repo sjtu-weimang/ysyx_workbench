211dcbdab22e63426686234a04b20f2fa7fc8104
tracer-ysyx <tracer@ysyx.org>
2025-04-09 23:11:09 +0800

    > sim RTL ysyx_22040000 张三 Linux zhang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:11:09 up 12:02,  1 user,  load average: 0.35, 0.81, 0.79

--- npc/csrc/difftest/dut.cpp
+++ npc/csrc/difftest/dut.cpp
@@ -1,26 +1,27 @@
 /***************************************************************************************
-* Copyright (c) 2014-2022 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2022 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include <cstdint>
 #include <dlfcn.h>
 
-#include "difftest-def.h"
-#include "../isa/isa.h"
 #include "../config.h"
+#include "../isa/isa.h"
+#include "difftest-def.h"
 
-void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
+void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n,
+                            bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
@@ -53,7 +54,7 @@ void difftest_skip_ref() {
 void difftest_skip_dut(int nr_ref, int nr_dut) {
   skip_dut_nr_inst += nr_dut;
 
-  while (nr_ref -- > 0) {
+  while (nr_ref-- > 0) {
     ref_difftest_exec(1);
   }
 }
@@ -65,36 +66,44 @@ void init_difftest(const char *ref_so_file, long img_size, int port) {
   handle = dlopen(ref_so_file, RTLD_LAZY);
   assert(handle);
 
-  ref_difftest_memcpy = decltype(ref_difftest_memcpy)(dlsym(handle, "difftest_memcpy"));
-  assert(ref_difftest_memcpy);
+  // ref_difftest_memcpy = decltype(ref_difftest_memcpy)(dlsym(handle,
+  // "difftest_memcpy")); assert(ref_difftest_memcpy);
 
-  ref_difftest_regcpy = (decltype(ref_difftest_regcpy))dlsym(handle, "difftest_regcpy");
+  ref_difftest_regcpy =
+      (decltype(ref_difftest_regcpy))dlsym(handle, "difftest_regcpy");
   assert(ref_difftest_regcpy);
 
-  ref_difftest_exec = decltype(ref_difftest_exec)(dlsym(handle, "difftest_exec"));
+  ref_difftest_exec =
+      decltype(ref_difftest_exec)(dlsym(handle, "difftest_exec"));
   assert(ref_difftest_exec);
 
-  ref_difftest_raise_intr = decltype(ref_difftest_raise_intr)(dlsym(handle, "difftest_raise_intr"));
-  assert(ref_difftest_raise_intr);
+  // ref_difftest_raise_intr =
+  //     decltype(ref_difftest_raise_intr)(dlsym(handle,
+  //     "difftest_raise_intr"));
+  // assert(ref_difftest_raise_intr);
 
-  void (*ref_difftest_init)(int) = decltype(ref_difftest_init)(dlsym(handle, "difftest_init"));
+  void (*ref_difftest_init)(int) =
+      decltype(ref_difftest_init)(dlsym(handle, "difftest_init"));
   assert(ref_difftest_init);
 
   Log("Differential testing: %s", ANSI_FMT("ON", ANSI_FG_GREEN));
   Log("The result of every instruction will be compared with %s. "
-      "This will help you a lot for debugging, but also significantly reduce the performance. "
-      "If it is not necessary, you can turn it off in menuconfig.", ref_so_file);
+      "This will help you a lot for debugging, but also significantly reduce "
+      "the performance. "
+      "If it is not necessary, you can turn it off in menuconfig.",
+      ref_so_file);
 
   ref_difftest_init(port);
   const uint32_t RESET_VECTOR = CONFIG_MBASE;
-  ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
+  ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size,
+                      DIFFTEST_TO_REF);
   auto cpu = get_current_cpu_state();
   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
 }
 
 static void checkregs(CPU_state *ref, vaddr_t pc, vaddr_t npc) {
   if (!isa_difftest_checkregs(ref, pc, npc)) {
-    npc_status=NPC_STATUS_FAILED;
+    npc_status = NPC_STATUS_FAILED;
     isa_reg_display();
   }
 }
@@ -109,14 +118,16 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
       checkregs(&ref_r, pc, npc);
       return;
     }
-    skip_dut_nr_inst --;
+    skip_dut_nr_inst--;
     if (skip_dut_nr_inst == 0)
-      panic("can not catch up with ref.pc = " FMT_WORD " at pc = " FMT_WORD, ref_r.pc, pc);
+      panic("can not catch up with ref.pc = " FMT_WORD " at pc = " FMT_WORD,
+            ref_r.pc, pc);
     return;
   }
 
   if (is_skip_ref) {
-    // to skip the checking of an instruction, just copy the reg state to reference design
+    // to skip the checking of an instruction, just copy the reg state to
+    // reference design
     auto cpu = get_current_cpu_state();
     ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
     is_skip_ref = false;
@@ -127,8 +138,8 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
 
   checkregs(&ref_r, pc, npc);
-  //Log("Difftest Passs");
+  // Log("Difftest Passs");
 }
 #else
-void init_difftest(char *ref_so_file, long img_size, int port) { }
+void init_difftest(char *ref_so_file, long img_size, int port) {}
 #endif
