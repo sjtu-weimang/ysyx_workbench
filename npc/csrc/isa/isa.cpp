81a947f8b71e3cd764b2885e1386290940de3780
tracer-ysyx <tracer@ysyx.org>
2025-04-10 00:19:20 +0800

    > sim RTL ysyx_22040000 张三 Linux zhang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:19:20 up 13:10,  1 user,  load average: 1.08, 0.89, 0.79

--- npc/csrc/isa/isa.cpp
+++ npc/csrc/isa/isa.cpp
@@ -149,6 +149,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc, vaddr_t npc) {
              i, pc, ref_r->gpr[i], cpu.gpr[i]);
       result = false;
     }
+    printf("ref reg[%d]: %0x", i, ref_r->gpr[i]);
   }
   return result;
 }
