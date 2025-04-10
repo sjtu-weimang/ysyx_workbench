549a3653f98c8ac8d65c1fc2b0481298a790a238
tracer-ysyx <tracer@ysyx.org>
2025-04-10 23:37:43 +0800

    > sim RTL ysyx_22040000 张三 Linux zhang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:37:43 up 1 day, 12:29,  1 user,  load average: 7.85, 4.37, 2.38

--- npc/csrc/difftest/dut.cpp
+++ npc/csrc/difftest/dut.cpp
@@ -140,7 +140,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
 
   checkregs(&ref_r, pc, npc);
-  Log("Difftest Passs at pc %x", pc);
+  // Log("Difftest Passs at pc %x", pc);
 }
 #else
 void init_difftest(char *ref_so_file, long img_size, int port) {}
