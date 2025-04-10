16955f29c33fd652564d1a9bfe6fa2b9661c992d
tracer-ysyx <tracer@ysyx.org>
2025-04-10 18:38:18 +0800

    > sim RTL ysyx_22040000 张三 Linux zhang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:38:18 up 1 day,  7:29,  1 user,  load average: 1.13, 1.12, 1.03

--- npc/csrc/config.h
+++ npc/csrc/config.h
@@ -2,11 +2,12 @@
 
 #define CONFIG_MBASE 0x80000000L
 
+#define CONFIG_TRACE
+
 #ifdef CONFIG_TRACE
 
 #define CONFIG_ITRACE
 #define CONFIG_DTRACE
 #define CONFIG_DIFFTEST
 #define CONFIG_MTRACE
-#define CONFIG_FTRACE
 #endif
