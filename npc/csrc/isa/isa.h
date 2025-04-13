83948827fb0293d61a7cdff9b00913752c726c3e
tracer-ysyx <tracer@ysyx.org>
2025-04-13 21:51:42 +0800

    > sim RTL ysyx_22040000 张三 Linux zhang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:51:42 up 24 min,  1 user,  load average: 2.13, 1.98, 1.52

--- npc/csrc/isa/isa.h
+++ npc/csrc/isa/isa.h
@@ -5,7 +5,7 @@
 #include "VTopLevel.h"
 #include <verilated.h>
 
-constexpr int MEM_SIZE = 1024 * 1024 * 30;
+constexpr int MEM_SIZE = 1024 * 1024 * 40;
 extern uint8_t mem[MEM_SIZE];
 
 extern const std::unique_ptr<VerilatedContext> contextp;
