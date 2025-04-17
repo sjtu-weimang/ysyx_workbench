d7ead25b33e486eb69a89228c0f3c0939b7315ee
tracer-ysyx <tracer@ysyx.org>
2025-04-18 00:20:22 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:20:22 up  1:27,  1 user,  load average: 1.08, 0.80, 0.69

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -143,13 +143,7 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
         break;
 
       default:
-        putch(state);
-        putch(state);
-        putch(state);
-        putch(state);
-        putch(state);
-        putch('\n');
-        assert(0);
+        state = 0;
       }
       state = 0;
       break;
