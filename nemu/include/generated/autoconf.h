a5199c938d2539de325017eb199301c57c221eea
tracer-ysyx <tracer@ysyx.org>
2025-04-17 14:34:24 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:34:24 up 19:04,  1 user,  load average: 0.71, 0.60, 0.63

--- nemu/include/generated/autoconf.h
+++ nemu/include/generated/autoconf.h
@@ -38,6 +38,7 @@
 #define CONFIG_SB_SIZE 0x10000
 #define CONFIG_CC "gcc"
 #define CONFIG_DIFFTEST_REF_PATH "none"
+#define CONFIG_CC_DEBUG 1
 #define CONFIG_TRACE_START 0
 #define CONFIG_DISK_IMG_PATH ""
 #define CONFIG_CC_GCC 1
