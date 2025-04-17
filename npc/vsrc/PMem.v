b9c3955edb6cfe4bc464de7c52c41591ac42de28
tracer-ysyx <tracer@ysyx.org>
2025-04-17 13:36:34 +0800

    > compile NEMU ysyx_22040000 张三 Linux zhang 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:36:34 up 18:06,  1 user,  load average: 0.59, 0.78, 0.72

--- npc/vsrc/PMem.v
+++ npc/vsrc/PMem.v
@@ -22,3 +22,42 @@ module PMem(
 	  end
 	end
 endmodule
+
+
+`ifdef SYNTHESIS  // 综合模式：用真实 Memory
+module PMem(
+		input valid,
+		input [31:0] raddr,
+		input [31:0] waddr,
+		input [31:0] wdata,
+		input [7:0] wmask,
+		input wen,
+		output reg [31:0] rdata
+		);
+    reg [31:0] mem [0:1023];
+    always @(posedge clk) begin
+        if (valid) begin
+            rdata <= mem[raddr[11:2]];
+            if (wen) begin
+                if (wmask[0]) mem[waddr[11:2]][7:0]   <= wdata[7:0];
+                if (wmask[1]) mem[waddr[11:2]][15:8]  <= wdata[15:8];
+                if (wmask[2]) mem[waddr[11:2]][23:16] <= wdata[23:16];
+                if (wmask[3]) mem[waddr[11:2]][31:24] <= wdata[31:24];
+            end
+        end
+        else begin
+            rdata <= 0;
+        end
+    end
+`else  // 仿真模式：用 DPI-C
+    always @(*) begin
+        if (valid) begin
+            rdata = pmem_read(raddr);
+            if (wen) pmem_write(waddr, wdata, wmask);
+        end
+        else begin
+            rdata = 0;
+        end
+    end
+`endif
+endmodule
